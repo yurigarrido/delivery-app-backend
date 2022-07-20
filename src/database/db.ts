32ffba4cca420e6853c8212509d2a36020606541
Yuri Garrido <y.garrido.yuri@gmail.com>
2022-07-20 19:30:50 -0300

    fix: db connection

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,14 +1,38 @@
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USERNAME,
-  process.env.PASSWORD_POSTGRES,
-  {
-    host: process.env.HOST,
-    dialect: "postgres",
-    port: +process.env.DB_PORT,
+// const db = new Sequelize({
+//   database: process.env.DATABASE,
+//   username: process.env.DB_USERNAME,
+//   password: process.env.PASSWORD_POSTGRES,
+//   host: process.env.HOST,
+//   port: +process.env.DB_PORT,
+//   dialect: "postgres",
+//   dialectOptions: {
+//     require: true,
+//     rejectUnauthorized: false,
+//     encrypt: "true"
+//   },
+// });
+
+// export default db;
+const Sequelize = require('sequelize');
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
-export default db;
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  export default db;
